526ce884f958c9d8460a588e5615ce155f83f722
Orkhand4 <orkhand4@mail.ru>
2024-10-19 12:16:29 +0400

    useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [debt, setDebt] = useState(0);
 
-  const [name, setName] = useState('Fuad Mikayilzade');
-  // getValue, setName, initialName
-
-  const demo = () => {
-    setName('Fatya Ahmedova')
+  function demo(e) {
+    let num = e.target.value 
+    setDebt(num)
   }
 
   return (
-
     <div>
-      <h1>Fuadin aci hekayesi</h1>
-      <h2>Rollarda: {name}</h2>
-      <button onClick={demo}>Rollari deyish</button>
-    </div>
-  )
-}
+      <h1>Fuadin aci Sigorta hekayesi</h1>
 
-export default App
+      <input onChange={demo} placeholder="debt" />
+
+      <h2>Borc: {debt}</h2>
+    </div>
+  );
+};
+
+export default App;
