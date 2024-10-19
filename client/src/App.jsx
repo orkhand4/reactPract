e7889d5895282de2351838482f97e767a1ca414c
Orkhand4 <orkhand4@mail.ru>
2024-10-20 03:20:22 +0400

    useState filter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,84 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './style.scss'
 
 const App = () => {
-  const [debt, setDebt] = useState(0);
 
-  function demo(e) {
-    let num = e.target.value 
-    setDebt(num)
-  }
+  const students = [
+    {
+      name: 'Yasemen',
+      surname: 'Memmedova',
+      age: 22,
+      isWorking: true,
+      hasVisa: false,
+    },
+    {
+      name: 'Jabbar',
+      surname: 'Jabbarli',
+      age: 23,
+      isWorking: false,
+      hasVisa: true,
+    },
+    {
+      name: 'Fatime',
+      surname: 'Ehmedova',
+      age: 21,
+      isWorking: true,
+      hasVisa: true,
+    },
+    {
+      name: 'Fuad',
+      surname: 'Mikayilzade',
+      age: 18,
+      isWorking: false,
+      hasVisa: false,
+    },
+    {
+      name: 'Orxan',
+      surname: 'Eliyev',
+      age: 28,
+      isWorking: false,
+      hasVisa: true,
+    },
+    {
+      name: 'Elxan',
+      surname: 'Meherremli',
+      age: 19,
+      isWorking: true,
+      hasVisa: true,
+    },
+  ];
+
+  const [search, setSearch] = useState('')
+  
+  const inptChange = (e) => {
+    setSearch(e.target.value)
+  };
 
   return (
-    <div>
-      <h1>Fuadin aci Sigorta hekayesi</h1>
+    <div className="page">
 
-      <input onChange={demo} placeholder="debt" />
+      
+      <input
+        onChange={inptChange}
+        type="text"
+        placeholder="Find..."
+      />
 
-      <h2>Borc: {debt}</h2>
+
+      <section className="demo">
+        <div className="calculator-wrapper">
+          {students.filter(item=> item.name.toUpperCase().includes(search.toUpperCase())).map((item, index) => {
+            return (
+              <div className="workerCard">
+                <h2>
+                  FulName: {item.name} {item.surname}
+                </h2>
+                <h2>Is working: {item.isWorking} </h2>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 };
